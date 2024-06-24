994dd9dbb2b43427f4380665db3787c00452f0f9
Alieza Ahmadi Veshki <alirezaahmadiveshki@gmail.com>
2024-06-24 21:34:14 +0330

    terminal based edition

--- app.py
+++ app.py
@@ -3,42 +3,15 @@ import os # working with files in Uni directory
 # import customtkinter # GUI designing
 from classes import *
 
-def fetch_add_book(lib_name, book_list):
-    """this function turn book-list to book objects and add them to the library with <lib-name>
-
-    :param lib_name: the library file path that the book-list is from   
-    :type lib_name: String
-    :param book_list: list of the lines of the library files in uni directory
-    :type book_list: List[str]
-    """
-    book_objects = []
-    for book in book_list:
-        if book.strip():
-            book = book.split()
-            b = Book(book[0], book[1], book[2], book[3])
-            book_objects.append(b)
-    
-    lib_name_list = lib_name.split("-")
-    id = lib_name_list[0]
-    name = lib_name_list[1][:-4]
-    lib = Library(name, id, book_objects)
-    Uni.libraries[lib.name] = lib
-    
 
 def fetch():
-    """fetching and reading data from librariy files located in the Uni directory
-    """
-    def library_file_path():
-        """opens library files then make a book-list of them and pass them to fetch_add_book function
-        """
-        mother_dir = R"C:\Users\PSK\Documents\GitHub\library-management\library-management\Uni"
-        libaries_List = os.listdir(mother_dir)
-        for library_name in libaries_List:
-            library_path = f"{mother_dir}/{library_name}"
-            with open(library_path, "r") as library_file:
-                book_list = library_file.readlines()
-                fetch_add_book(library_name, book_list)
-    library_file_path()
+    libaries_List = os.listdir(Uni.mother_dir)
+    for library_name in libaries_List:
+        l = library_name.split("-")
+        id = l[0]
+        name = l[1][:-4]
+        library = Library(name, id)
+        Uni.libraries[name] = library
 
 
 def make_window():
@@ -47,183 +20,69 @@ def make_window():
     print("-" * 40)
 
 
-def requset_gathering(university):
-    """prints out the user interface in the termainal
-
-    :return: inp: input that is gathered    
-    :rtype: String
-    """
-    university.show_lib()
-    print("-" * 40)
-    print("choose on of the below options :")
-    print("BOOKS:")
-    print("\t1 for ADD a book")
-    print("\t2 for DELETING a book")
-    print("\t3 for EDITING A BOOK")
-    print("\t4 for book info")
-    print("libraries:")
-    print("\t5 for ADD a library")
-    print("\t6 for DELETING a library")
-    print("\t7 for library info")
-    print("\t0 for the QUITING")
-    inp = input("choose your request: ")
-    return inp
-
-
-def request_validation(inp):
-    """controls that if the user input is validated or not
-
-    :param inp: the input of this function is the output of the request_gathering function
-    :type inp: String
-    :raises ValueError: if the inp wasn't an integer between 0 to 7
-    :return: the exact function with diffrent input that we have in the except block
-    :rtype: Funcion
-    """
-    try:
-        int_inp = int(inp)
-        if 0 <= int_inp <= 7:
-            return int_inp
-        else:
-            raise ValueError
-    except:
-        print("-" * 40)
-        print("wrong input\nplease try again")
-        print("-" * 40)
-        inp = requset_gathering(university)
-        return request_validation(inp)
-
-
 def book_add(university):
-    # library, name, rel_date, author, genre
-    while True:
-        library = input("Library: ")
-        name = input("Book's Name: ")
-        rel_date = input("Book's Release date: ")
-        author = input("Book's Author/writer: ")
-        genre = input("Books's genre: ")
-        b = Book(name, rel_date, author, genre)
-        if library in university.libraries.keys():
-            lib = university.libraries[library]
-            lib.add_book(b)
-            break
-        else:
-            print("wrong library name, try again")
+    lib_name = input("library's name: ")
+    book_name = input("book's name: ")
+    rel_date = input("book's release date: ")
+    author = input("book's author: ")
+    genre = input("book's genre: ")
+    lib = Uni.find_lib(lib_name)
+    b = Book(lib_name, book_name, rel_date, author, genre)
+    lib.add_book(b)
 
 
 def book_del(university):
-    while True:
-        library = input("Library: ")
-        name = input("Book's Name: ")
-        if library in university.libraries.keys():
-            lib = university.libraries[library]
-            for b in lib.books:
-                if b.name == name:
-                    lib.remove_book(b)
-                    break
-            break
-        else:
-            print("wrong library name, try again")    
-
-
-# def changing_book_info(lib, book):
-#     print("which title do you want to edit?")
-#     print("1 for name\n2 for release data\n3 for author\n4 for genre")
-#     opt = input("option: ").lower()
-#     if opt == "1":
-#         new_name = input("enter the new name: ")
-#         lib.edit_book(book, new_name , book.rel_date, book.author, book.genre)
-#     elif opt == "2":
-#         new_release_date = input("enter the new release date: ")
-#         lib.edit_book(book, book.name , new_release_date, book.author, book.genre)
-#     elif opt == "3":
-#         new_author = input("enter the new author: ")
-#         lib.edit_book(book, book.name , book.rel_date, new_author, book.genre)
-#     elif opt == "4":
-#         new_genre = input("enter the new genre: ")
-#         lib.edit_book(book, book.name , book.rel_date, book.author, new_genre)
-#     else:
-#         print("wrong option please try again")
+    lib_name = input("library's name: ")
+    lib = Uni.find_lib(lib_name)
+    book_name = input("book's name: ")
+    book = lib.find_book(book_name)
+    lib.remove_book(book)
+    
 
 
 def book_edit(university):   
-    print("*"*40) 
-    print("this command is under process")
-    print("*"*40) 
-    # libs = university.libraries.values()
-    # book_name = input("Book's name: ")
-    # for lib in libs:
-    #     lib_books = lib.books
-    #     for book in lib_books:
-    #         if book.name == book_name:
-    #             changing_book_info(lib, book)
-    #             break
-    #         print("*" * 40)
-    #         print("there's not a book with this name")
-    #         opt = input("enter r for resuming and q for quiting: ").lower()
-    #         if opt == "r":
-    #             book_edit(university)
-    #         elif opt == "q":
-    #             return
-    #         else:
-    #             print("wrong input, try again")    
-    #         print("*" * 40)
-    #     break
+    lib_name = input("library's name: ")
+    lib = Uni.find_lib(lib_name)
+    lib.lib_info() 
+    book_name = input("book's name: ")
+    book = lib.find_book(book_name)
+    book.book_info()
+    print("fill the detail that you want to change\n"
+          "and skip the otherones")
+    new_name = input("new name: ")
+    new_rel_date = input("new release date: ")
+    new_author = input("new author: ")
+    new_genre = input("new genre: ")
+    lib.edit_book(book, new_name, new_rel_date, new_author, new_genre)
+
+
 
             
 
 def book_info(university):
-    """print out the book's information with the name of the book
-
-    :param university: the university object
-    :type university: Uni
-    """
-    inp_name = input("book's name: ")
-    a = False
-    for lib in university.libraries.values():
-        for book in lib.books:
-            if book.name == inp_name.lower():
-                print("*"* 40)
-                print(lib.name)
-                print("*"* 40)
-                book.book_info()
-                a = True
-    if a == False:
-        print("-" * 40)
-        print("this book doesn't exist")
-        print("-" * 40)
+    ...
 
 
 def lib_add(university):
-    print("*" * 40)
-    print("*" * 40)
-    print("fill the follow informations")
     name = input("library's name: ")
     id = input("library's id: ")
-    lib = Library(name, id)
-    university.add_lib(lib)
+    university.add_lib(Library(name, id))
 
 
 def lib_del(university):
-    try:
-        lib_name = input("library name: ")
-        lib = university.libraries[lib_name]
-        university.remove_lib(lib)
-    except KeyError:
-        print("wrong library name, try again")
+    name = input("library's name: ")
+    lib = university.libraries[name]
+    university.remove_lib(lib)
 
 
 def lib_info(university):
-    """prints out the library information
-
-    :param university: university object
-    :type university: Uni
-    """
-    lib_name = input("Library's name: ")
-    lib_object = university.libraries[lib_name]
-    lib_object.lib_info()
+    lib_name = input("library's name: ")
+    lib_name = lib_name.lower()
+    lib = Uni.find_lib(lib_name)
+    lib.lib_info()
 
 
-def request_pending(inp, university):
+def function_pending(university, inp):
     """the function links the user input to a function
 
     :param inp: the user input
@@ -243,14 +102,16 @@ def main():
     university = Uni()
     fetch()
     make_window()
-    print("hello dear user :)")
+    request = Request()
     while True:
-        inp = requset_gathering(university)
-        int_inp = request_validation(inp)
-        if int_inp == 0:
+        university.show_lib()
+        inp = request.gathering()
+        key, valid_inp = request.validation(inp)
+        if valid_inp == 0:
             break
-        request_pending(int_inp, university)
-        
+        if key:
+            function_pending(university, valid_inp)
+               
 
 if __name__ == "__main__":
     main()
--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 import os
+from pprint import pprint
+
 
 class Uni():
     """the university object
@@ -13,6 +15,13 @@ class Uni():
     # so we are sure that we can acsses to this addres throughout our code
 
 
+    @staticmethod
+    def find_lib(name):
+        for lib_name, lib in Uni.libraries.items():
+            if name.lower() == lib_name.lower():
+                return lib
+
+
     def add_lib(self, lib: "Library") -> None:
         """this method is doing:
         1 - adding a library to libraries attribute of the university object
@@ -21,9 +30,9 @@ class Uni():
         :param lib: a library object that we're adding
         :type lib: Library
         """
-        self.libraries[lib.name] = lib
-        with open(lib.path, "w") as lib_file:
-            lib_file.close()
+        Uni.libraries[lib.name] = lib
+        lib.file.write()
+
 
 
     def remove_lib(self, lib: "Library") -> None:
@@ -35,7 +44,8 @@ class Uni():
         :type lib: Library
         """
         del self.libraries[lib.name]
-        os.remove(lib.path) 
+        lib.file.remove_file()
+        
 
 
     def show_lib(self):
@@ -49,15 +59,20 @@ class Uni():
 
 
 class Library():
-    mother_dir = R"C:\Users\PSK\Documents\GitHub\library-management\library-management\Uni"
-    def __init__(self, name, id, books = []) -> None:
+    def __init__(self, name: str, id: int) -> None:
         self.name = name
         self.id = id
-        self.books = books
-        self.path = fR"{self.mother_dir}/{self.id}-{self.name}.txt"
+        self.books = []
+        self.path = fR"{Uni.mother_dir}/{self.id}-{self.name}.txt"
+        self.file = File(f"{self.id}-{self.name}.txt")
+        self.file.remove_spaces()
+        for line in self.file.lines:
+            char_list = line.strip().split(" ")
+            b = Book(self.name, char_list[0], char_list[1], char_list[2], char_list[3])
+            self.books.append(b)
 
 
-    def add_book(self, book):
+    def add_book(self, book: "Book") -> None:
         """this method is doing:
         1 - adds the input book to the book sattribute of hte library object
         2 - adds the information of the book to the library file with <id-name.txt> name  
@@ -66,37 +81,44 @@ class Library():
         :type book: Book
         """
         self.books.append(book)
-        with open(self.path, "a") as lib_file:
-            lib_file.write(f"\n{book.name} {book.rel_date} {book.author} {book.genre}")    
+        self.lib_info()
+        self.file.create_line(book)
 
 
-    def remove_book(self, book):
+    def remove_book(self, book: "Book") -> None:
         """removing a book object:
         from the list: library's books attribute
         from the library's file
         """
         self.books.remove(book) # removes the book from the books attribute of the library objects
-
-        with open(self.path, "r") as lib_file: # open library file
-            book_list = lib_file.readlines() # reading the lines of it
-            # removing the book from the book_list
-            for b in book_list:
-                if book.name in b:
-                    book_list.remove(b)
-                    lib_file.close()
-                    break
-        with open(self.path, "w") as lib_file:
-            lib_file.writelines(book_list)
+        self.lib_info()
+        self.file.remove_line(book.name)
+        self.file.remove_spaces()
 
             
 
-    def edit_book(self, book, new_name, new_rel_data, new_author, new_genre):
-        new_book = Book(new_name, new_rel_data, new_author, new_genre)
-        self.add_book(new_book)
+    def edit_book(self, book, new_name = "", new_rel_date = "", new_author = "", new_genre = ""):
+        if new_name == "":
+            new_name = book.name
+        if new_rel_date == "":
+            new_rel_date = book.rel_date
+        if new_author == "":
+            new_author = book.author
+        if new_genre == "":
+            new_genre = book.genre
+
         self.remove_book(book)
+        edited_book = Book(self.name, new_name, new_rel_date, new_author, new_genre)            
+        self.add_book(edited_book)
 
 
-    def lib_info(self):
+    def find_book(self, book_name):
+        for book in self.books:
+            if book.name.lower() == book_name.lower():
+                return book
+
+
+    def lib_info(self) -> None:
         """prints out the library information by it's name
         """
         print("-" * 40)
@@ -106,17 +128,129 @@ class Library():
         print("-" * 40)
 
 
+
+
 class Book():
-    def __init__(self, name, rel_date, author, genre) -> None:
+    def __init__(self, library, name, rel_date, author, genre) -> None:
         self.name = name.lower()
         self.rel_date = rel_date
         self.author = author.lower()
         self.genre = genre.lower()
+        self.library = library.lower()
+
 
 
     def book_info(self):
         """prints out the object book's information
         """
         print("-" * 40)
-        print(f"name: {self.name}\nrelease date: {self.rel_date}\nauthor/writer: {self.author}\ngenre: {self.genre}")
+        print(f"library: {self.library}\nname: {self.name}\nrelease date: {self.rel_date}\nauthor/writer: {self.author}\ngenre: {self.genre}")
         print("-" * 40)
+
+
+
+
+class File():
+    mother_dir = R"C:\Users\PSK\Documents\GitHub\library-management\library-management\Uni"
+    def __init__(self, name) -> None:
+        self.name = name
+        self.path = fR"{File.mother_dir}\{self.name}"
+        self.amount = 0
+        self.lines = []
+        f = open(self.path, 'a') #creates the file if it's not created
+        f.close()
+        with open(self.path, 'r') as file:
+            for line in file:
+                self.amount += 1
+                self.lines.append(line)
+        self.remove_spaces()
+
+
+    def show(self):
+        print(f"name: {self.name}\npath: {self.path}\namount: {self.amount}\n lines: {self.lines}")
+
+
+    def remove_spaces(self):
+        with open(self.path, 'r') as file:
+            self.lines = []
+            for line in file: 
+                if line != '\n':
+                    self.lines.append(line)
+        self.write()      
+
+
+    def remove_line(self, book_name):
+        for line in self.lines:
+            name = line.split(" ")[0]
+            if name.startswith('\n'):
+                name = name.strip()
+            if (name.lower() == book_name.lower()):
+                self.lines.remove(line)
+                self.write()
+                break
+        self.remove_spaces()
+
+
+    def write(self):
+        with open(self.path, "w") as file:
+            file.writelines(self.lines)       
+
+
+    def create_line(self, book):
+        self.lines.append(f"\n{book.name} {book.rel_date} {book.author} {book.genre}")
+        self.write()
+        self.remove_spaces()
+
+
+
+    def remove_file(self):
+        os.remove(self.path)
+        
+
+
+class Request():
+
+    def gathering(self):
+        """prints out the user interface in the termainal
+
+        :return: inp: input that is gathered    
+        :rtype: String
+        """
+        print("-" * 40)
+        print("choose on of the below options :")
+        print("BOOKS:")
+        print("\t1 for ADD a book")
+        print("\t2 for DELETING a book")
+        print("\t3 for EDITING A BOOK")
+        print("\t4 for book info")
+        print("libraries:")
+        print("\t5 for ADD a library")
+        print("\t6 for DELETING a library")
+        print("\t7 for library info")
+        print("\t0 for the QUITING")
+        inp = input("choose your request: ")
+        return inp
+    
+
+    def validation(self, inp):
+        """controls that if the user input is validated or not
+
+        :param inp: the input of this function is the output of the request_gathering function
+        :type inp: String
+        :raises ValueError: if the inp wasn't an integer between 0 to 7
+        :return: the exact function with diffrent input that we have in the except block
+        :rtype: Funcion
+        """
+        key = False
+        try:
+            int_inp = int(inp)
+            if 0 <= int_inp <= 7:
+                key = True
+                return (key, int_inp)
+            else:
+                raise ValueError
+        except Exception:
+            print("-" * 40)
+            print("wrong input\nplease try again")
+            print("-" * 40)
+            return key, inp
